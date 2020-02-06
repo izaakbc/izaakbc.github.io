c40e8e3e6179c23f7f0c8d4d751558aacdcc0f0d
izaakbc <59936552+izaakbc@users.noreply.github.com>
2020-02-06 11:33:12 -0700

    Update unitconvertercalc.js

--- unitconvertercalc.js
+++ unitconvertercalc.js
@@ -1,3 +1,59 @@
  function Convert(){
    alert("Results May Vary!")
  }
+
+ function Convert(){
+   alert("Results May Vary!")
+
+var input=document.getElementById("input").value
+var outputunit=document.getElementById("outputunit").text 
+var inputunit=document.getElementById("inputunit").text
+var Answer=document.getElementById("Answer").text
+
+
+ Answer.innerHTML= "<b>hello</b>"
+
+
+input = "Inches" && outputunit = "Feet"
+
+Inches to Feet	ft=in*0.083333	
+Inches to Meters	m=in/39.370	
+Inches to cm	cm=in/0.39370	
+Inches to Yards	yd=in*0.027778	
+Inches to Kilometers	km=in/39370	
+Inches to Miles	mi=in*0.000015783
+   
+Feet to Meters	m=ft/3.2808	
+Feet to Inches	in=ft*12	
+Feet to cm	cm=ft/0.032808	
+Feet to Yards	yd=ft*0.33333	
+Feet to Kilometers	km=ft/3280.8	
+Feet to Miles	mi=ft*0.00018939
+   
+Yards to Feet	ft=yd*3	
+Yards to Meters	m=yd/1.0936	
+Yards to Inches	in=yd*36	
+Yards to cm	cm=yd/0.010936	
+Yards to Kilometers	km=yd/1093.6	
+Yards to Miles	mi=yd*0.00056818
+   
+cm to Feet	ft=cm*0.032808	
+cm to Meters	m=cm/100	
+cm to Inches	in=cm*0.39370	
+cm to Yards	yd=cm*0.010936	
+cm to Kilometers	km=cm/100000	
+cm to Miles	mi=cm*0.0000062137
+   
+Meters to Feet	ft=m*3.2808	
+Meters to Inches	in=m*39.370	
+Meters to cm	cm=m/0.01	
+Meters to Yards	yd=m*1.0936	
+Meters to Kilometers	km=m/1000	
+Meters to Miles mi=m*0.00062137
+   
+Kilometers to Feet	ft=km*3280.8	
+Kilometers to Meters	m=km*1000	
+Kilometers to Inches	in=km*39370	
+Kilometers to cm	cm=km*100000	
+Kilometers to Yards	mi=km*1093.6	
+Kilometers to Miles	mi=km*0.62137 
