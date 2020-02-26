9448c859930dc7bd35ba025cebcd026cea0987d6
izaakbc <59936552+izaakbc@users.noreply.github.com>
2020-02-26 13:28:40 -0700

    Update LeafletTeam.js

--- LeafletTeam.js
+++ LeafletTeam.js
@@ -5,26 +5,26 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var Slope = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/S-687b640df0014a079c6ad87ab7296213/wms?', {
-layers: 'Countries_WGS84 Countries_WGS84', //Title name
+var Slope = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver//wms?', {
+layers: '', //Title name
 format: 'image/png', //depends on what you want to load in
 transparent: true,
 }).addTo(map);
   
 var DEM = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-687b640df0014a079c6ad87ab7296213/wms?', {
-layers: 'World_Cities World_Cities', //Title name
+layers: 'Roads_Shp', //Title name
 format: 'image/png', //depends on what you want to load in
 transparent: true,
 }).addTo(map);
   
-var Roads = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-687b640df0014a079c6ad87ab7296213/wms?', {
-layers: 'World_Cities World_Cities', //Title name
+var Roads = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver//wms?', {
+layers: '', //Title name
 format: 'image/png', //depends on what you want to load in
 transparent: true,
 }).addTo(map);
   
-var Water = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-687b640df0014a079c6ad87ab7296213/wms?', {
-layers: 'World_Cities World_Cities', //Title name
+var Water = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver//wms?', {
+layers: '', //Title name
 format: 'image/png', //depends on what you want to load in
 transparent: true,
 }).addTo(map);
