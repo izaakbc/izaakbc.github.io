1e55fb2fa8343e4d7af899b061867fd615a583a3
izaakbc <59936552+izaakbc@users.noreply.github.com>
2020-02-26 13:33:50 -0700

    Update LeafletTeam.js

--- LeafletTeam.js
+++ LeafletTeam.js
@@ -1,5 +1,6 @@
 function myFunction(){
-    var map = L.map('map').setView([45.5, -122.6], 6);
+    var map = L.map('map').setView([45.5, -122.6], 6
+                                );
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -11,21 +12,21 @@ format: 'image/png', //depends on what you want to load in
 transparent: true,
 }).addTo(map);
   
-var DEM = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-6ade1ad2379045368d1f6d83801f087d/wms?', {
-layers: 'Roads_Shp', //Title name
+var DEM = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver//wms?', {
+layers: '', //Title name
 format: 'image/png', //depends on what you want to load in
 transparent: true,
 }).addTo(map);
   
-var Roads = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver//wms?', {
-layers: '', //Title name
-format: 'image/png', //depends on what you want to load in
+var Roads = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver/HS-6ade1ad2379045368d1f6d83801f087d/wms?', {
+layers: 'Roads_Shp', 
+format: 'image/png', 
 transparent: true,
 }).addTo(map);
   
 var Water = L.tileLayer.wms('https://geoserver.hydroshare.org/geoserver//wms?', {
-layers: '', //Title name
-format: 'image/png', //depends on what you want to load in
+layers: '', 
+format: 'image/png', 
 transparent: true,
 }).addTo(map);
 
